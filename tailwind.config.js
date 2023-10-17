9863efc4d7df738f6125f5f901721cab9c74d81e
Isaias Buscarino <isaias.buscarino@hotmail.com>
2023-10-17 23:05:50 +0200

    Configuring fonts on tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,19 @@ module.exports = {
   content: [
     './app/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './app/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
