52ecd99ef0c73a88134f8b5a06653dc6aa4aff81
Isaias Buscarino <isaias.buscarino@hotmail.com>
2023-10-17 23:03:43 +0200

    Fix relative path alias

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,20 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: './'
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
-}
+  moduleNameMapper: {
+    '^utils(.*)$': '<rootDir>/app/utils$1',
+    '^components(.*)$': '<rootDir>/app/components$1',
+    '^styles(.*)$': '<rootDir>/app/styles$1'
+  }
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
