e43444a223963004be7d588c03ef7650e97acd67
Isaias Buscarino <isaias.buscarino@hotmail.com>
2024-03-10 22:21:08 +0100

    Install and configure shadcn

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React from "react";
 import type { Preview } from "@storybook/react";
 
-import { withThemeByClassName } from "@storybook/addon-styling";
+import { withThemeByClassName } from '@storybook/addon-themes';
 
+import { ThemeProvider } from '../components/theme-provider';
 import { localPoppins } from '../app/fonts';
 
 /* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
 import "../app/globals.css";
+import { useTheme } from "next-themes";
+
+export const globalTypes = {
+  theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'dark',
+      toolbar: {
+          icon: 'circlehollow',
+          items: ['light', 'dark'],
+          showName: false
+      }
+  }
+};
+
+const withThemeProvider = (Story, context) => {
+  const { setTheme } = useTheme();
+
+  console.log('context.globals.theme: ', context.globals.theme)
+  if (context.globals.theme) {
+    setTheme(context.globals.theme);
+  }
+
+  return (
+    <ThemeProvider
+        attribute="class"
+        defaultTheme={context.globals.theme}
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+const withFonts = (Story) => {
+  return <div className={localPoppins.className}><Story /></div>;
+}
 
 const preview: Preview = {
   parameters: {
@@ -17,6 +56,19 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: 'dark',
+      values: [
+          {
+              name: 'light',
+              value: 'hsl(var(--background))'
+          },
+          {
+              name: 'dark',
+              value: 'hsl(var(--background))'
+          }
+      ]
+    }
   },
 
   decorators: [
@@ -27,11 +79,10 @@ const preview: Preview = {
         light: "light",
         dark: "dark",
       },
-      defaultTheme: "light",
+      defaultTheme: "dark",
     }),
-    (Story) => {
-      return <div className={localPoppins.className}><Story /></div>;
-    }
+    withThemeProvider,
+    withFonts
   ],
 };
 
